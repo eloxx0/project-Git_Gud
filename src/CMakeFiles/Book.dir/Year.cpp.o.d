73ae24a8c6bf83e218892c48c394defdd243d794
eloxx0 <71978500+eloxx0@users.noreply.github.com>
2023-11-22 16:50:48 +0100

    .gitignore added

--- src/CMakeFiles/Book.dir/Year.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/Book.dir/Year.cpp.o: \
- /home/elo/lab_programmazione/PROGETTO_INTERMEDIO/Book/src/Year.cpp \
- /usr/include/stdc-predef.h \
- /home/elo/lab_programmazione/PROGETTO_INTERMEDIO/Book/src/../include/Year.h
