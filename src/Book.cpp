07196b8e8ca835c371c958b1013b6964026ee9a4
Fr33st <150927075+Fr33st@users.noreply.github.com>
2023-11-23 15:42:39 +0100

    ADDED BOOK.CPP

---
+++ src/Book.cpp
@@ -0,0 +1,17 @@
+#include  "../include/Book.h"
+
+std::string Book::getTitle(void){
+	return title;
+}
+
+std::string Book::getName(void){
+	return name;
+}
+
+std::string Book::getSurname(void){
+	return surname;
+}
+
+int Book::getDate(void){
+	return date;
+}
