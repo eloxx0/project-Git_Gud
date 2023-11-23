87d821a53d41cb2e4c93cf942b85719a71092ee1
Fr33st <150927075+Fr33st@users.noreply.github.com>
2023-11-23 15:43:47 +0100

    ADDED BOOK.H

--- include/Book.h
+++ include/Book.h
@@ -4,9 +4,26 @@
 #include "Date.h"
 #include <iostream>
 #include "Year.h"
+#include <string>
 
 class Book{
 
+	private:
+		
+	std::string title;
+	std::string name;
+	std::string surname;
+	int date;
+	bool onloan;
+	bool available;
+	
+	public:
+	
+	std::string getTitle(void);
+	std::string getName(void);
+	std::string getSurname(void);
+	int getDate(void);
+	
 };
 
 #endif // BOOK
