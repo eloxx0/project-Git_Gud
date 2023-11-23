402cf18bd4fd6ed14462e9d1e9d6e1c0127b2ee7
Fr33st <150927075+Fr33st@users.noreply.github.com>
2023-11-23 16:04:46 +0100

    book.cpp

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,6 +1,6 @@
 #include  "../include/Book.h"
 
-Book::Book(std::string nome, std::string cognome, std::string titolo): name{nome}, surname{cognome}, title{titolo}{/*sistemare*/}
+Book::Book(std::string nome, std::string cognome, std::string titolo): name{nome}, surname{cognome}, title{titolo}{}
 
 Book::Book(std::string nome, std::string cognome, std::string titolo, int data): name{nome}, surname{cognome}, title{titolo}, date{data}{}
 
