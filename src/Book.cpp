018061284640edd7549e809bc43a567da84b91aa
Fr33st <150927075+Fr33st@users.noreply.github.com>
2023-11-23 18:05:25 +0100

    modifiche cpp

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,21 +1,37 @@
 #include  "../include/Book.h"
 
-Book::Book(std::string nome, std::string cognome, std::string titolo): name{nome}, surname{cognome}, title{titolo}{}
+Book::Book(std::string nome, std::string cognome, std::string titolo): name{nome}, surname{cognome}, title{titolo}{
+	available=true;
+}
 
-Book::Book(std::string nome, std::string cognome, std::string titolo, int data): name{nome}, surname{cognome}, title{titolo}, date{data}{}
+Book::Book(std::string nome, std::string cognome, std::string titolo, int data): name{nome}, surname{cognome}, title{titolo}, date{data}{
+	available=true;
+}
 
-std::string Book::getTitle(void){
+std::string Book::getTitle(void) const{
 	return title;
 }
 
-std::string Book::getName(void){
+std::string Book::getName(void) const{
 	return name;
 }
 
-std::string Book::getSurname(void){
+std::string Book::getSurname(void) const{
 	return surname;
 }
 
-int Book::getDate(void){
+int Book::getDate(void) const{
 	return date;
+}
+
+bool Book::loan(void){
+	available=false;
+	
+	return available;
+}
+
+bool Book::present(void){
+	available=true;
+	
+	return available;
 }
