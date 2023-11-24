9990d3ada0fab7d5d0617b672ca5a1e7e57ded75
eloxx0 <71978500+eloxx0@users.noreply.github.com>
2023-11-24 11:12:27 +0100

    updated Book.h

--- include/Book.h
+++ include/Book.h
@@ -1,10 +1,9 @@
-#ifndef BOOK
-#define BOOK
+#ifndef BOOK_H
+#define BOOK_H
 
 #include "Date.h"
-#include <iostream>
-#include "Year.h"
 #include <string>
+#include "Isbn.h"
 
 class Book{
 
@@ -13,23 +12,44 @@ class Book{
 	std::string title;
 	std::string name;
 	std::string surname;
-	int date;
+	Date date;
+    Isbn isbn;
 	bool available;
 	
 	public:
 	
-	Book(std::string nome, std::string cognome, std::string titolo);
-	Book(std::string nome, std::string cognome, std::string titolo, int data);
+    //quando un oggetto di tipo book viene creato, la variabile available viene assegnata true, poichè il libro
+    //risulta come disponibile per essere preso in prestito
+	Book(std::string nome, std::string cognome, std::string titolo, Isbn isbn);
+	Book(std::string nome, std::string cognome, std::string titolo, Isbn isbn, Date data);
 	
 	//getter
-	std::string getTitle(void) const;
-	std::string getName(void) const;
-	std::string getSurname(void) const;
-	int getDate(void) const;
+	std::string getTitle(void) const{
+        return title;
+    };
+	std::string getName(void) const{
+        return name;
+    };
+	std::string getSurname(void) const{
+        return surname;
+    };
+	Date getDate(void) const{
+        return date;
+    };
+    bool getAvailability() const{
+        return available;
+    }
+    Isbn getIsbn() const{
+        return isbn;
+    }
+    //permette di cambiare lo stato di disponibilità di un libro
+    void set_availability(bool b);
 	
-	bool loan(void);
-	bool present(void);
 	
 };
+//permette di prendere in prestito un libro e quindi di cambiare la sua disponibilità
+void borrow_book(Book& book);
 
-#endif // BOOK
+void return_book(Book& book);
+
+#endif // BOOK_H
