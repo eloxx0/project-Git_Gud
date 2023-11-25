20408005c226edd5003579cf65ea4013b95210e2
Fr33st <150927075+Fr33st@users.noreply.github.com>
2023-11-25 01:59:36 +0100

    modifiche ulteriori

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,5 +1,8 @@
 #include  "../include/Book.h"
 
+Book::Book():name{""}, surname{""}, title{""}, isbn{}, date{}{}
+//gli ultimi due dati membro essendo udt richiamo il costruttore vuoto definito nella loro classe, i primi 3 dati membro sono stringhe vuote
+
 Book::Book(std::string nome, std::string cognome, std::string titolo, Isbn isbn): name{nome}, surname{cognome}, title{titolo}, isbn{isbn}{
 	available=true;
 }
