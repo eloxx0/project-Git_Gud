c6c6586f3f5b69413d8e7b0b4907c0cb1dbe4dc9
Fr33st <150927075+Fr33st@users.noreply.github.com>
2023-11-26 23:19:24 +0100

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include "../include/Date.h"
-#include "../include/Year.h"
-#include "../include/Isbn.h"
-#include <iostream>
+#include "../include/Bookshelf.h"
+
 int main(){
 
     Date a{15, Month(3), 2000};
     std::cout << a;
     Date b;
     std::cout << b;
+    
+    
 }
